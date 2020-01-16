617aa93f919dee5bec8628ef9ffed28ce0e21a4a
Jon Borgonia <jon.borgonia@gmail.com>
2020-01-15 20:57:32 -0800

    captured the noisy flag 🏳️
    packets are filtered, ordered, then assembled

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
     ]
   }
 */
-const { readFileSync } = require('fs')
+const { readFileSync, writeFileSync } = require('fs')
 const { join } = require('path')
 const { parseGlobalHeader, parsePcapPacket } = require('./pcap')
 
@@ -39,21 +39,70 @@ const globalPcapHeader = parseGlobalHeader(pcapData)
 // start offset for packets = 24
 const firstPacketOffset = 24
 const pcapRecordHeaderSize = 16
+const httpBodyDelimiter = Buffer.from('\r\n\r\n')
+
+const isServer = ({
+  ethernetFrame: {
+    userData: {
+      data: {
+        sourcePort
+      }
+    }
+  }
+}) => sourcePort === 80
+
+const isACK = ({
+  ethernetFrame: {
+    userData: {
+      data: {
+        flags
+      }
+    }
+  }
+}) => flags.includes('ACK') && !flags.includes('SYN')
+
+const orderBySeq = (tcpA, tcpB) =>
+  tcpA.ethernetFrame.userData.data.seq -
+  tcpB.ethernetFrame.userData.data.seq
 
 const getNextPacket = (packets, offset) => {
   let packet = parsePcapPacket(pcapData, offset)
   if (packet == null) return packets
 
-  // @TODO filter
-
   return getNextPacket([...packets, packet], offset+packet.length+pcapRecordHeaderSize)
 }
 
 const packets = getNextPacket([], firstPacketOffset)
+  .filter(isServer)
+  .filter(isACK)
+  .sort(orderBySeq)
+  .reduce(({uniquePackets, seqs}, {
+    ethernetFrame: {
+      userData: {
+        data
+      }
+    }
+  }) => {
+    if(seqs.has(data.seq)) return { uniquePackets, seqs }
+
+    seqs.add(data.seq)
+    return {
+      uniquePackets: [...uniquePackets, data],
+      seqs
+    }
+  }
+  , { uniquePackets: [], seqs: new Set() })
+  .uniquePackets
+  .reduce((httpResponse, data) =>
+    Buffer.concat([httpResponse, data.data])
+  , Buffer.from([]))
+
 
 const parsedPcapFile = {
   globalPcapHeader,
-  packets
+  packets: packets.slice(packets.indexOf(httpBodyDelimiter)+httpBodyDelimiter.length)
 }
 
 console.log(JSON.stringify(parsedPcapFile, null, 2))
+
+writeFileSync('out.jpg', parsedPcapFile.packets)
